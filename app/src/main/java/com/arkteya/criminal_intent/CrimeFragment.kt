2a3b02e010484420d0ce6861c5236791eae931d9
LidiyaShamalo <arkteya07@gmail.com>
2025-03-30 15:09:41 +0300

    Creating a branch with generated data

--- app/src/main/java/com/arkteya/criminal_intent/CrimeFragment.kt
+++ app/src/main/java/com/arkteya/criminal_intent/CrimeFragment.kt
@@ -3,7 +3,6 @@ package com.arkteya.criminal_intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +10,6 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import java.util.UUID
-
-private const val ARG_CRIME_ID = "crime_id"
-private const val TAG = "CrimeFragment"
 
 class CrimeFragment : Fragment() {
 
@@ -26,15 +18,9 @@ class CrimeFragment : Fragment() {
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
 
-    private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
-        ViewModelProvider(this)[CrimeDetailViewModel::class.java]
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
-        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
-        crimeDetailViewModel.loadCrime(crimeId)
     }
 
     override fun onCreateView(
@@ -43,40 +29,31 @@ class CrimeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
-        titleField = view.findViewById<EditText>(R.id.crime_title)!!
-        dateButton = view.findViewById<Button>(R.id.crime_date)!!
-        solvedCheckBox = view.findViewById<CheckBox>(R.id.crime_solved)!!
+
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
         dateButton.apply {
             text = crime.date.toString()
             isEnabled = false
         }
-        return view
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        crimeDetailViewModel.crimeLiveData.observe(
-            viewLifecycleOwner,
-            Observer { crime ->
-                crime?.let {
-                    this.crime = crime
-                    updateUI()
-                }
-            }
-        )
+        return view
     }
 
     override fun onStart() {
         super.onStart()
 
         val titleWatcher = object : TextWatcher {
+
             override fun beforeTextChanged(
                 sequence: CharSequence?,
                 start: Int,
                 count: Int,
                 after: Int
             ) {
+                // This space intentionally left blank
             }
 
             override fun onTextChanged(
@@ -89,34 +66,15 @@ class CrimeFragment : Fragment() {
             }
 
             override fun afterTextChanged(sequence: Editable?) {
-
+                // This one too
             }
         }
+
         titleField.addTextChangedListener(titleWatcher)
 
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
-            }
-        }
-    }
-
-    private fun updateUI(){
-        titleField.setText(crime.title)
-        dateButton.text = crime.date.toString()
-        solvedCheckBox.apply {
-            isChecked = crime.isSolved
-            jumpDrawablesToCurrentState()
-        }
-    }
-
-    companion object {
-        fun newInstance(crimeId: UUID): CrimeFragment {
-            val args = Bundle().apply {
-                putSerializable(ARG_CRIME_ID, crimeId)
-            }
-            return CrimeFragment().apply {
-                arguments = args
             }
         }
     }
--- app/src/main/java/com/arkteya/criminal_intent/MainActivity.kt
+++ app/src/main/java/com/arkteya/criminal_intent/MainActivity.kt
@@ -1,39 +1,24 @@
 package com.arkteya.criminal_intent
 
+
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.util.UUID
 
-private  const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(),
-CrimeListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
 
-        if(currentFragment == null) {
+        if (currentFragment == null) {
             val fragment = CrimeListFragment.newInstance()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
         }
-    }
-
-    override fun onCrimeSelected(crimeId: UUID) {
-        val fragment = CrimeFragment.newInstance(crimeId)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null) // добавление фрагмента с подробностями в обратны стек, т.е. для нажатия кнопки "Назад"
-            .commit()
     }
 }
