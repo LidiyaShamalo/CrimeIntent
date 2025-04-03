cd41484ed72cbd0f5834ce06ea0d350781d27dc8
LidiyaShamalo <arkteya07@gmail.com>
2025-04-03 18:56:12 +0300

    Using Implicit Intent - Phone Book

--- app/src/main/java/com/arkteya/criminal_intent/database/CrimeDatabase.kt
+++ app/src/main/java/com/arkteya/criminal_intent/database/CrimeDatabase.kt
@@ -4,6 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.arkteya.criminal_intent.Crime
 import java.util.Date
 import java.util.UUID
@@ -11,11 +13,19 @@ import java.util.UUID
 
 private const val DATABASE_NAME = "crime-database"
 
-@Database(entities = [ Crime::class ], version=1, exportSchema = false)
+@Database(entities = [ Crime::class ], version=2, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 
     abstract fun crimeDao(): CrimeDao
+}
+
+val migration_1_2 = object: Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(
+            "ALTER TABLE Crime ADD COLUMN suspect TEXT NOT NULL DEFAULT ''"
+        )
+    }
 }
 
 
